034dc9fe9b76dab7e4f911b053b4a035840f557a
laurent andriamifidy <laurent.an@helloasso.com>
2024-03-14 15:03:37 +0100

    add httpx and pokemon url

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
+import httpx
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 
 def index(request: HttpRequest) -> HttpResponse:
+    url = "https://pokeapi.co/api/v2/pokemon/1/"
     context = {}
     return render(request,"core/index.html", context=context)
