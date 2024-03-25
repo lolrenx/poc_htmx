e87dd6d29b2a077f8b9ed652ebfc3665187f1b26
laurent andriamifidy <laurent.an@helloasso.com>
2024-03-25 09:54:20 +0100

    replace httpx w niquests, for science

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-import httpx
+import niquests as re
 
 from django.contrib import messages
 from django.shortcuts import render
@@ -7,5 +7,6 @@ from django.http import HttpResponse, HttpRequest
 
 def index(request: HttpRequest) -> HttpResponse:
     url = "https://pokeapi.co/api/v2/pokemon/1/"
-    context = {}
+    data = re.get(url).json()
+    context = {"data": data}
     return render(request,"core/index.html", context=context)
