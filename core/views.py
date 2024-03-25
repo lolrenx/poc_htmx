654a5369bff9fac330ee13103e9b45dd9f92c02c
laurent andriamifidy <laurent.an@helloasso.com>
2024-03-25 10:02:05 +0100

    get many

--- core/views.py
+++ core/views.py
@@ -7,6 +7,12 @@ from django.http import HttpResponse, HttpRequest
 
 def index(request: HttpRequest) -> HttpResponse:
     url = "https://pokeapi.co/api/v2/pokemon/1/"
-    data = re.get(url).json()
+    data = []
+    with re.Session(multiplexed=True) as s:
+        for i in range(1, 150):
+            url = f"https://pokeapi.co/api/v2/pokemon/{i}/"
+            r = s.get(url)
+            data.append(r.json())
+
     context = {"data": data}
     return render(request,"core/index.html", context=context)
